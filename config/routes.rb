45cc59f9181b36c574676b4e4ee35de93e75592e
KralMarko123 <kralmarko123@gmail.com>
2026-04-20 15:25:28 +0200

    chore: add route for env check

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "stories#page"
   get "/info", to: "stories#show"
+  get "/envcheck", to: proc { [ 200, { "Content-Type" => "text/plain" }, [ "Rails.env=#{Rails.env}" ] ] }
 end
