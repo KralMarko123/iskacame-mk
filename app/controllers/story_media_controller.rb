90d7c19f8f67e5cdbe7d4342f848bbd7dc37a044
KralMarko123 <kralmarko123@gmail.com>
2026-04-23 11:16:24 +0200

    feat: add favicon and serve files through rails

---
+++ app/controllers/story_media_controller.rb
@@ -0,0 +1,31 @@
+class StoryMediaController < ApplicationController
+  def show
+    filename = File.basename(params[:filename].to_s)
+    path = Rails.root.join("public", "story_cache", filename)
+
+    return head :not_found unless File.file?(path)
+
+    send_file(
+      path,
+      disposition: "inline",
+      type: content_type_for(filename)
+    )
+  end
+
+  private
+
+  def content_type_for(filename)
+    case File.extname(filename).downcase
+    when ".png"
+      "image/png"
+    when ".jpg", ".jpeg"
+      "image/jpeg"
+    when ".webp"
+      "image/webp"
+    when ".mp4"
+      "video/mp4"
+    else
+      "application/octet-stream"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "stories#page"
   get "/info", to: "stories#show"
+  get "/story_cache/:filename", to: "story_media#show", constraints: { filename: /[^\/]+/ }
 
   namespace :internal do
     resource :story_cache, only: :create
